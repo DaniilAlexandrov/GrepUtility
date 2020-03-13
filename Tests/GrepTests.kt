e00e80178537e9d6197860afae886b6c26066339
WorryWarrior <worrywarrior1@gmail.com>
2020-03-13 20:15:55 +0300

    Oh, hello.

--- Tests/GrepTests.kt
+++ Tests/GrepTests.kt
@@ -1,9 +1,62 @@
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
 
-class GrepTests {
+internal class GrepUtilTest {
+
+    private val output = ByteArrayOutputStream()
+
+    @BeforeEach
+    fun startStream() {
+        System.setOut(PrintStream(output))
+    }
+
+    @AfterEach
+    fun clearStream() {
+        System.setOut(null)
+    }
+
     @Test
-    fun test() {
-
+    fun noParameters() {
+        Grep.main(arrayOf("kotlin", "Tests\\Test.txt"))
+        assertEquals("kotlin\r\n", output.toString())
+    }
+    @Test
+    fun rParameter() {
+        Grep.main(arrayOf("-r", "kotlin", "Tests\\Test.txt"))
+        assertEquals("kotlin\r\n", output.toString())
+    }
+    @Test
+    fun vParameter() {
+        Grep.main(arrayOf("-v", "kotlin", "Tests\\Test.txt"))
+        assertEquals("Kotlin KOtlin\r\nKOTLIN \r\nQQotlin\r\nKotlinExemplar\r\n", output.toString())
+    }
+    @Test
+    fun iParameter() {
+        Grep.main(arrayOf("-i", "kotlin", "Tests\\Test.txt"))
+        assertEquals("Kotlin KOtlin\r\nkotlin\r\nKOTLIN \r\nKotlinExemplar\r\n", output.toString())
+    }
+    @Test
+    fun irParameters() {
+        Grep.main(arrayOf("-r", "-i", "qqotlin", "Tests\\Test.txt"))
+        assertEquals("QQotlin\r\n", output.toString())
+    }
+    @Test
+    fun ivParameters() {
+        Grep.main(arrayOf("-v", "-i", "ot", "Tests\\Test.txt"))
+        assertEquals("\r\n", output.toString())
+    }
+    @Test
+    fun rvParameters() {
+        Grep.main(arrayOf("-v", "-r", "kotlin", "Tests\\Test.txt"))
+        assertEquals("Kotlin KOtlin\r\nKOTLIN \r\nQQotlin\r\nKotlinExemplar\r\n", output.toString())
+    }
+    @Test
+    fun rivParameters() {
+        Grep.main(arrayOf("-i","-v", "-r", "[\\w+]otlin", "Tests\\Test.txt"))
+        assertEquals("Kotlin KOtlin\r\nKOTLIN \r\nQQotlin\r\nKotlinExemplar\r\n", output.toString())
     }
 }
--- src/Grep.kt
+++ src/Grep.kt
@@ -1,10 +1,66 @@
-import java.util.regex.Matcher
 import java.util.regex.Pattern
 import org.kohsuke.args4j.Option
+import org.kohsuke.args4j.Argument
+import org.kohsuke.args4j.CmdLineException
+import org.kohsuke.args4j.CmdLineParser
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
 
 class Grep {
-    @Option(name = "-r", metaVar = "Regex", usage = "Passes a regular expression")
-    private val regex = false
+    companion object {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        Grep().passArguments(args)
+    }
+}
+    @Option(name = "-r", metaVar = "Regex", usage = "Sets a regular expression")
+    private var regex = false
 
+    @Option(name = "-v", metaVar = "Invert", usage = "Reverts regex condition")
+    private var invert = false
 
+    @Option(name = "-i", metaVar = "Ignore", usage = "Ignores passed text's case")
+    private var ignore = false
+
+    @Argument(required = true, metaVar = "Word", usage = "Regex word requirement")
+    private var word: String? = null
+
+    @Argument(required = true, metaVar = "Document", index = 1, usage = "Document to filter")
+    private var file: File? = null
+
+private fun flagDeterminant(line: String): Boolean {
+    val res = if (!regex) {
+        if (!ignore)
+            line.contains(word!!)
+        else
+        line.toUpperCase().contains(word!!.toUpperCase())
+        } else {
+        val pattern = Pattern.compile(word!!)
+        val matcher = pattern.matcher(line)
+        if (!ignore)
+            matcher.matches()
+        else
+            matcher.matches() || pattern.matcher(line.toUpperCase()).matches() ||
+                    pattern.matcher(line.toLowerCase()).matches()
+    }
+    return invert != res
+}
+
+private fun passArguments(args: Array<String>) {
+    val argsParser = CmdLineParser(this)
+    try {
+        argsParser.parseArgument(*args)
+    } catch (e: CmdLineException) {
+        println(e.message)
+        println("Required format is: java -jar GrepUtility.jar -v Invert -i Ignore_Register -r Regex word input_name.txt")
+        argsParser.printUsage(System.err)
+        return
+    }
+    val matchingLines = BufferedReader(FileReader(file!!)).use { it1 -> it1.readLines().filter { flagDeterminant(it) }  }
+    if (matchingLines.isEmpty())
+    println("")
+    else
+        println(matchingLines.joinToString(separator = "\r\n"))
+    }
 }
